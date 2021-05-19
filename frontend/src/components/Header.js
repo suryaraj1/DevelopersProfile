bc6ffc5255dbc82f9ca7413e87d777f2b8e04a26
suryaraj1 <suryaraj_bhaduri@yahoo.com>
2021-05-19 23:18:40 +0530

    added headers

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+import HeaderImage from "../static/undraw_dev_productivity_umsq 1.png";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="header-upper">
+          <div className="header-text">
+            <p>The Developer</p>
+            <p>Repository</p>
+          </div>
+          <div className="header-image">
+            <img src={HeaderImage} />
+          </div>
+        </div>
+        <div className="header-lower">
+          <p>Explore developer profiles</p>
+        </div>
+        <hr className="header-divider" />
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="search-bar">
+        <input type="input" placeholder="Search for username" />
+      </div>
+    );
+  }
+}
+
+export default Searchbar;
