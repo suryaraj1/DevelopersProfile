92d2a18b9bff92c33eed4def290676836c732f61
suryaraj1 <suryaraj_bhaduri@yahoo.com>
2021-05-22 09:11:53 +0530

    added navbar links

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 class Navbar extends React.Component {
   render() {
     return (
       <div className="navbar-section">
-        <p className="navbar-left-section">The Developer Profile</p>
-        <p className="navbar-right-section">All Developers</p>
+        <Link className="link" to="/">
+          <p className="navbar-left-section">The Developer Profile</p>
+        </Link>
+        <Link className="link" to="/">
+          <p className="navbar-right-section">All Developers</p>
+        </Link>
       </div>
     );
   }
